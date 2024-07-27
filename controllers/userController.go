b0d51f30db3a27ac50b119b3e52672a8ec8380f3
Vaibhav <sahusv4527@gmail.com>
2024-07-27 23:49:47 +0530

    login

--- controllers/userController.go
+++ controllers/userController.go
@@ -5,12 +5,14 @@ import (
 	"golang-jwt/database"
 	helper "golang-jwt/helpers"
 	"golang-jwt/models"
+	"log"
 	"net/http"
 	"time"
 
 	"github.com/Vaibhavsahu2810/golang-jwt/models"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"golang.org/x/crypto/bcrypt"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -19,6 +21,15 @@ import (
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 var validate = validator.New()
+
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	return string(bytes)
+}
 
 func GetUser() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
@@ -74,11 +85,14 @@ func Signup() gin.HandlerFunc {
 				"error": err.Error(),
 			})
 		}
+
 		if count > 0 {
 			c.JSON(http.StatusConflict, gin.H{
 				"error": "Email already exists",
 			})
 		}
+		password := HashPassword(*user.Password)
+		user.Password = &password
 		count1, err := userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
 		defer cancel()
 		if err != nil {
@@ -109,6 +123,55 @@ func Signup() gin.HandlerFunc {
 		defer cancel()
 
 		c.JSON(http.StatusCreated, result)
+
+	}
+}
+
+func VerifyPassword(userPassword string, providedPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
+	check := true
+	msg := ""
+	if err != nil {
+		msg = "Wrong Password"
+		check = false
+	}
+	return check, msg
+}
+
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 30*time.Second)
+
+		var user models.User
+		var foundUser models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid request",
+			})
+		}
+		defer cancel()
+		err := userCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Invalid email or password",
+			})
+		}
+		passwordIsValid, msg := VerifyPassword(*user.Password, *&foundUser.Password)
+		defer cancel()
+		if !passwordIsValid {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": msg,
+			})
+		}
+		token, refreshToken, _ := helper.GenerateAllTokens(*foundUser.Email, *foundUser.First_name, *foundUser.Last_name, *foundUser.User_type, foundUser.User_id)
+		helper.UpdateAllTokens(token, refreshToken, foundUser.User_id)
+		err = userCollection.FindOne(ctx, bson.M{"user_id": foundUser.User_id}).Decode(&foundUser)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, foundUser)
 
 	}
 }
--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,7 +1,15 @@
 package helper
 
 import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/akhil/golang-jwt-project/database"
 	jwt "github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type SignedDetails struct {
@@ -11,4 +19,67 @@ type SignedDetails struct {
 	Uid        string
 	User_type  string
 	jwt.StandardClaims
+}
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var secretKey = []byte("secretkey")
+
+func GenerateAllTokens(email string, firstName string, lastName string, userType string, uid string) (signedToken string, signedRefreshToken string, err error) {
+
+	claims := &SignedDetails{
+		Email:      email,
+		First_name: firstName,
+		Last_name:  lastName,
+		Uid:        uid,
+		User_type:  userType,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+
+	refreshClaims := &SignedDetails{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168)).Unix(),
+		},
+	}
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(SECRET_KEY))
+
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	return token, refreshToken, err
+
+}
+func UpdateAllTokens(signedToken string, signedRefreshToken string, userId string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+	updateObj := bson.D{
+		{Key: "token", Value: signedToken},
+		{Key: "refresh_token", Value: signedRefreshToken},
+		{Key: "updated_at", Value: time.Now().Format(time.RFC3339)},
+	}
+
+	filter := bson.M{"user_id": userId}
+
+	upsert := true
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+
+	result, err := userCollection.UpdateOne(
+		ctx,
+		filter,
+		bson.D{{Key: "$set", Value: updateObj}},
+		&opt,
+	)
+
+	if err != nil {
+		log.Panicf("Failed to update tokens for user %s: %v", userId, err)
+		return
+	}
+
+	log.Printf("Updated tokens for user %s: %v", userId, result)
 }
